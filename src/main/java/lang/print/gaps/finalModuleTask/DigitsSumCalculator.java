76e2e45d365541e114b37f20519bd8addf96bd36
Skorpiyo <skorpio277@gmail,com>
2022-09-21 23:34:23 +0300

    reverter and printer was so hard for me

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int calculateSum;
+        for (calculateSum = 0; number > 0; calculateSum = calculateSum + number % 10, number = number / 10);
+        System.out.println(calculateSum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int revert = 0;
+        for( ;number != 0; number=number/10){
+            int remainder = number % 10;
+        revert = revert * 10 + remainder;}
+        System.out.println(revert);
     }
 }
